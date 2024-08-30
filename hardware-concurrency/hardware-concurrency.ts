0e19731e81c8a61e420fc7ea69877e82f643a7b3
Daniel Cardenas <danielcardeenas@gmail.com>
2024-08-30 13:45:24 -0700

    Use default value type in concurrency

--- hardware-concurrency/hardware-concurrency.ts
+++ hardware-concurrency/hardware-concurrency.ts
@@ -1,18 +1,22 @@
-let initialHardwareConcurrency: {
-  unsupported: boolean;
-  numberOfLogicalProcessors?: number;
-};
-if (typeof navigator !== "undefined" && "hardwareConcurrency" in navigator) {
-  initialHardwareConcurrency = {
-    unsupported: false,
-    numberOfLogicalProcessors: navigator.hardwareConcurrency,
-  };
-} else {
-  initialHardwareConcurrency = { unsupported: true };
-}
 const useHardwareConcurrency = (defaultParams?: {
   numberOfLogicalProcessors: number;
 }) => {
+  let initialHardwareConcurrency: {
+    unsupported: boolean;
+    numberOfLogicalProcessors: number;
+  };
+  if (typeof navigator !== "undefined" && "hardwareConcurrency" in navigator) {
+    initialHardwareConcurrency = {
+      unsupported: false,
+      numberOfLogicalProcessors: navigator.hardwareConcurrency,
+    };
+  } else {
+    initialHardwareConcurrency = {
+      unsupported: true,
+      numberOfLogicalProcessors: defaultParams?.numberOfLogicalProcessors ?? 1,
+    };
+  }
+
   return { ...defaultParams, ...initialHardwareConcurrency };
 };
 
